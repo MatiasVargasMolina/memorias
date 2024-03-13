7282e3a760391c79270dfed3b523d97cc16e0e2f
matias <matias.vargas.m@usach.cl>
2024-03-13 07:18:02 -0600

    cambios

--- src/components/Texto.jsx
+++ src/components/Texto.jsx
@@ -2,17 +2,6 @@ import React from 'react'
 
 function Texto() {
   return (
-    <div className='flex-col items-center w-full'>
-      <a href="#" class=" flex h-3/4 pt-8 mt-8 overflow-hidden whitespace-no-wrap">
-        <div className='pl-4 ml-4 flex flex-col h-screen items-start'>
-          <h1 className='mb-4 text-4xl font-play ml-4 '>MEMORIAS</h1>
-          <h2 className='mb-4 text-3xl font-play ml-4 '>Del lat. memoria.</h2>
-          <h2 className='mb-4 text-3xl  font-play ml-4 '>1.f.Facultad psíquica por medio de la cual se retiene y recuerda el pasado.</h2>
-          <h2 className='mb-4 text-3xl font-play ml-4 '>Recorrer la memoria</h2>
-          <h2 className='mb-4 text-3xl font-play ml-4 '>renovar la memoria</h2>
-          <h2 className='mb-4 text-3xl font-play ml-4 '>2.loc. verb. Recordar de nuevo los asuntos ya pasados..</h2></div>
-      </a>
-    </div>
   )
 }
 
